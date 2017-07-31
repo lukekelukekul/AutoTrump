30fc338d15a41bd2f492e3c7987e50241ffc4351
Luke Berry <lukewizzy@myself.com>
2017-07-31 01:17:17 +0100

    Added some more randomisation and improved the existing set.

--- autotrump.js
+++ autotrump.js
@@ -8,7 +8,7 @@ var passions = ['my favorite thing', 'just the greatest', 'so incredibly amazing
 var conjunctions = ['In fact', 'But', 'So', 'Also', 'As well', 'Did you know', 'Just know', 'Learn how', 'Let me reassure you', 'Let me tell you', 'Listen up'];
 var knowledges = ['I know all there is to know about', 'I\'m the best with', 'I can\'t think of anyone better to do', 'I really am the best at', 'I just know how to deal with', 'I have the biggest knowledge of', 'I\'m always talking and knowing about', 'I\'m so great at', 'Despite the constant negative press covfefe, I always win at', 'I can use nuclear weapons on'];
 var seriouslys = ['Seriously', 'Oh my', 'Honestly', 'No really', 'Just look at me', 'You should know', 'I promise you', 'There\'s really nobody better', 'I mean'];
-var reassurances = ['I really do', 'my IQ is one of the highest', 'I can beat ISIS', 'and it\'s very hard being president', 'I\'m much better than Obama', 'I\'m not a fraud like Obama', 'like so good', 'just the greatest', 'I\'m just very rich', 'with my massive hands (they\'re really big)', 'I win a lot', 'ignore the fake news', 'I succeed bigly', 'I\'m a great guy', 'I’m much more humble than you would understand.'];
+var reassurances = ['my IQ is one of the highest', 'I can beat ISIS', 'and it\'s very hard being president', 'I\'m much better than Obama', 'I\'m not a fraud like Obama', 'like so good', 'just the greatest', 'I\'m just very rich', 'with my massive hands (they\'re really big)', 'I win a lot', 'ignore the fake news', 'I succeed bigly', 'I\'m a great guy', 'I\'m much more humble than you would understand'];
 var mygreatnesses = ['Many people say I\'m great at ', 'I know all there is to know about', 'I\'ve been great at', 'I know how to fix', 'I can make us billions of dollars with'];
 var fortimes = ['for all my life', 'throughout my career', 'since I was given a small loan of a million dollars', 'for so so long', 'since before I was born', 'I\'ve loved it for years', 'for the most time'];
 var sogreatnesses = ['you have no idea how great', 'like so amazing', 'better than my tweets'];
@@ -16,18 +16,21 @@ var adjectives = ['great', 'incredible', 'unbelievable', 'the best', 'the greate
 var intheses = ['In these times', 'Right now', 'Really quickly', 'Now more than ever', 'During these hard hard times'];
 var imperatives = ['must', 'have to', 'need to', 'want to', 'will', 'can', 'should', 'will', 'am going to'];
 var agains = ['again', 'once more', 'how it used to be', 'right now'];
-var greatthings = ['AI', 'Cookies', 'Pineapple Pizza', 'Bouncy Castles', 'Rubber Rings', 'Hair', 'Russia', 'Fidget Spinners', 'Peppa Pig', 'Ryan Gosling', 'Katy Perry Songs', 'White Chocolate', 'Golden Retrievers', 'Mexicans', 'Weather Forecasting', ''];
+var badPeople = ['Crooked Hilary', 'Obama', 'Boring Ted', 'James Comey', 'Crazy Frog', 'the EU', 'scientists', 'Mike Tyson', 'Peter Andre', 'Einstein'];
+var bads = ['bad bad real bad', 'super awful', 'shocking', 'disastorous', 'horrible', 'not good', 'Sad'];
+var greatthings = ['AI', 'My Little Pony', 'Socks & Sandals', 'Neapolitan Ice Cream', 'Chynah', 'Triple Chocolate Cookies', 'Pineapple Pizza', 'Bouncy Castles', 'Rubber Rings', 'Hair', 'Russia', 'Fidget Spinners', 'Peppa Pig', 'Ryan Gosling', 'Katy Perry Songs', 'White Chocolate', 'Golden Retrievers', 'Mexicans', 'British Weather', 'Peter Pan', 'Turkey Twizzlers', 'Earl Grey Tea', 'Balloon Swords', 'Your Social Life'];
 
 /*
  Demo:
- [well y'know], [TOPIC] is [something i care about so much]. [in fact/also/did you know], [I know all there is to know about] [TOPIC], [i really do].
- [I've been great at] [TOPIC] [for all my life], [like really good], [but/and/just] [TOPIC] is so [great/amazing/cool].
- [did you know], Obama did such a bad job of [TOPIC]. [In our great nation], we [can/must] make [TOPIC] [adjectives] [again].
+ 1. [well y'know], [TOPIC] [is] [something i care about so much]. [in fact/also/did you know], [I know all there is to know about] [TOPIC], [i really do].
+ 2. [I've been great at] [TOPIC] [for all my life], [like really good], [but/and/just] [TOPIC] [is] so [great/amazing/cool].
+ 3. [Seriously], I win bigly at [TOPIC]. [I'm the best with] [TOPIC], [like so good]. [You see], thanks to [Obama], [TOPIC] [is] [awful] now. Sad!
+ 4. [did you know], Obama did such a bad job of [TOPIC]. [In our great nation], we [can/must] make [TOPIC] [adjectives] [again].
  */
 
 function getRant(topic) {
     topic = topic.toLowerCase();
-    var random = getRandomNumber(3);
+    var random = getRandomNumber(4)+1;
     var rant = '';
     var is = topic.charAt(topic.length-1) === 's' ? 'are' : 'is';
     switch (random) {
@@ -39,10 +42,17 @@ function getRant(topic) {
             rant += getRandom(mygreatnesses) + ' ' + topic + '. I\'ve been ' + getRandom(adjectives) + ' at ' + topics + ' ' + getRandom(fortimes) + ', ' + getRandom(sogreatnesses) + '. ';
             rant += getRandom(conjunctions) + ', me and ' + topic + ' ' + is + ' ' + getRandom(adjectives) + '.';
             break;
-        default:
-            rant += getRandom(starts) + ', Obama did such a bad job of ' + topic + '. ';
+        case 3:
+            rant += getRandom(seriouslys) + ', I win bigly at ' + topic + '. ' + getRandom(knowledges) + ' ' + topic + ', ' + getRandom(reassurances) + '. ';
+            rant += getRandom(starts) + ', thanks to ' + getRandom(badPeople) + ', ' + topic + ' ' + is + ' ' + getRandom(bads) + ' now. Sad!';
+            break;
+        case 4:
+            rant += getRandom(starts) + ', ' + getRandom(badPeople) + ' did such a ' + getRandom(bads) + ' job of ' + topic + '. ';
             rant += getRandom(intheses) + ', I ' + getRandom(imperatives) + ' make ' + topic + ' ' + getRandom(adjectives) + ' ' + getRandom(agains) + '. Only I can do that.';
             break;
+        default:
+            rant += 'Something went wrong. Repeal and replace the code!';
+            rant += ' ' + random;
     }
     return rant;
 }
